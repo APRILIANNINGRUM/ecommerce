7b6a2a6fda09d7476ae5f379fa542f7e6f0e3371
APRILIANNINGRUM <111201912033@mhs.dinus.ac.id>
2023-03-14 17:12:08 +0700

    day7

--- resources/views/layouts/ecommerce/module/menu.blade.php
+++ resources/views/layouts/ecommerce/module/menu.blade.php
@@ -1,3 +1,122 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    @yield('title')
+    <meta name="description" content="Hutche Furniture eCommerce Template is a wonderful and elegant-looking website template that is the best choice for any type of online furniture store."/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="https://gmpg.org/xfn/11">
+    <link rel="canonical" href="Replace_with_your_PAGE_URL" />
+    
+    <!-- Open Graph (OG) meta tags are snippets of code that control how URLs are displayed when shared on social media  -->
+    <!-- <meta property="og:locale" content="en_US" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="Be Furniture – About Us" />
+    <meta property="og:url" content="PAGE_URL" />
+    <meta property="og:site_name" content="Be Furniture – About Us" /> -->
+    <!-- For the og:image content, replace the # with a link of an image -->
+    <!-- <meta property="og:image" content="#" />
+    <meta property="og:description" content="Be Furniture – About Us" /> -->
+    
+    <!-- Add site Favicon -->
+    <link rel="icon" href="https://hasthemes.com/wp-content/uploads/2019/04/cropped-favicon-32x32.png" sizes="32x32" />
+    <link rel="icon" href="https://hasthemes.com/wp-content/uploads/2019/04/cropped-favicon-192x192.png" sizes="192x192" />
+    <link rel="apple-touch-icon" href="https://hasthemes.com/wp-content/uploads/2019/04/cropped-favicon-180x180.png" />
+    <meta name="msapplication-TileImage" content="https://hasthemes.com/wp-content/uploads/2019/04/cropped-favicon-270x270.png" />
+    @yield('css')
+          <!-- CSS 
+    ========================= -->
+    <!-- <link rel="stylesheet" href="assets/css/vendor/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/swiper-bundle.min.css">
+    <link rel="stylesheet" href="assets/css/slick.css">
+    <link rel="stylesheet" href="assets/css/ionicons.min.css">
+    <link rel="stylesheet" href="assets/css/font.awesome.css">
+    <link rel="stylesheet" href="assets/css/icofont.min.css">
+    <link rel="stylesheet" href="assets/css/elegant-icons.min.css">
+    <link rel="stylesheet" href="assets/css/animate.css">
+    <link rel="stylesheet" href="assets/css/nice-select.css">
+    <link rel="stylesheet" href="assets/css/magnific-popup.css">
+    <link rel="stylesheet" href="assets/css/jquery-ui.min.css"> -->
+    <link rel="stylesheet" href="{{ asset('furnitures/assets/css/vendor/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('furnitures/assets/css/swiper-bundle.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('furnitures/assets/css/slick.css')}}">
+    <link rel="stylesheet" href="{{ asset('furnitures/assets/css/ionicons.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('furnitures/assets/css/font.awesome.css')}}">
+    <link rel="stylesheet" href="{{ asset('furnitures/assets/css/icofont.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('furnitures/assets/css/elegant-icons.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('furnitures/assets/css/animate.css')}}">
+    <link rel="stylesheet" href="{{ asset('furnitures/assets/css/nice-select.css')}}">
+    <link rel="stylesheet" href="{{ asset('furnitures/assets/css/magnific-popup.css')}}">
+    <link rel="stylesheet" href="{{ asset('furnitures/assets/css/jquery-ui.min.css')}}">
+
+
+    <!-- Main Style CSS -->
+    <!-- <link rel="stylesheet" href="assets/css/style.css"> -->
+    <link rel="stylesheet" href="{{ asset('furnitures/assets/css/style.css')}}">
+    
+    <!--modernizr min js here-->
+    <!-- <script src="assets/js/vendor/modernizr-3.7.1.min.js"></script> -->
+    <script src="{{ asset('furnitures/assets/js/vendor/modernizr-3.7.1.min.js')}}"></script>
+
+    <!-- Structured Data  -->
+    @yield('js')
+
+	
+</head>
+
+<body>
+<div class="body_overlay">
+
+    </div>
+    <div class="offcanvas_menu">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="offcanvas_menu_wrapper">
+                        <div class="canvas_close">
+                            <a href="javascript:void(0)"><i class="ion-android-close"></i></a>
+                        </div>
+                        <div id="menu" class="text-left ">
+                            <ul class="offcanvas_main_menu">
+                                <li class="menu-item-has-children active">
+                                    <a href="#">Home</a>
+                                    <ul class="sub-menu">
+                                        <li><a href="index.html">Home 1</a></li>
+                                        <li><a href="index-2.html">Home 2</a></li>
+                                    </ul>
+                                </li>
+                                <li class="menu-item-has-children">
+                                    <a href="#">Pages</a>
+                                    <ul class="sub-menu">
+                                        <li><a href="about.html">About Us</a></li>
+                                        <li><a href="contact.html">Contact Us</a></li>
+                                    </ul>
+                                </li>
+                                <li class="menu-item-has-children">
+                                    <a href="#">Shop</a>
+                                    <ul class="sub-menu">
+                                        <li><a href="shop-left-sidebar.html">shop left sidebar</a></li>
+                                        <li><a href="shop-fullwidth.html">shop fullwidth</a></li>
+                                        <li><a href="shop-collection.html">shop collection</a></li>
+                                    </ul>
+                                </li>
+                                <li class="menu-item-has-children"><a href="single-product.html">Product</a></li>
+                                <li class="menu-item-has-children">
+                                    <a href="#">blog</a>
+                                    <ul class="sub-menu">
+                                        <li><a href="blog.html">blog</a></li>
+                                        <li><a href="blog-details.html">blog details</a></li>
+                                    </ul>
+                                </li>
+                                <li class="menu-item-has-children"><a href="contact.html">Contact Us</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 <header class="header_section mb-30">
         <div class="container">
             <div class="row">
@@ -80,4 +199,41 @@
 
     </header>
     <!--mini cart-->
+
+    <script src="{{ asset('furnitures/assets/js/vendor/jquery-3.4.1.min.js') }}"></script>
+    <script src="{{ asset('furnitures/assets/js/vendor/popper.js') }}"></script>
+    <script src="{{ asset('furnitures/assets/js/vendor/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('furnitures/assets/js/swiper-bundle.min.js') }}"></script>
+    <script src="{{ asset('furnitures/assets/js/slick.min.js') }}"></script>
+    <script src="{{ asset('furnitures/assets/js/wow.min.js') }}"></script>
+    <script src="{{ asset('furnitures/assets/js/jquery.scrollup.min.js') }}"></script>
+    <script src="{{ asset('furnitures/assets/js/images-loaded.min.js') }}"></script>
+    <script src="{{ asset('furnitures/assets/js/jquery.nice-select.js') }}"></script>
+    <script src="{{ asset('furnitures/assets/js/jquery.magnific-popup.min.js') }}"></script>
+    <script src="{{ asset('furnitures/assets/js/mailchimp-ajax.js') }}"></script>
+    <script src="{{ asset('furnitures/assets/js/jquery.counterup.min.js') }}"></script>
+    <script src="{{ asset('furnitures/assets/js/jquery-waypoints.js') }}"></script>
+    <script src="{{ asset('furnitures/assets/js/jquery-ui.min.js') }}"></script>
+    <script src="{{ asset('furnitures/assets/js/ajax-mail.js') }}"></script>
+    <script>
+        $.ajax({
+            url: "{{route ('customer.totalCart')}}",
+            type: 'GET',
+            success: function(data) {
+                $('.total').html(data);
+            }
+        });
+    </script>
+
+
+    <!-- Main JS -->
     
+    <!-- <script src="assets/js/main.js"></script> -->
+    @yield('js')
+    <script src="{{ asset('furnitures/assets/js/main.js') }}"></script>
+   
+
+    
+</body>
+
+</html>
